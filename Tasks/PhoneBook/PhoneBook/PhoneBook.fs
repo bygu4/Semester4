2b6101cc1f01bbc3de6ad065ab4aa32e155ad716
bygu4 <sacha05bugaev@gmail.com>
2025-03-11 23:49:05 +0300

    add phonebook solution

---
+++ Tasks/PhoneBook/PhoneBook/PhoneBook.fs
@@ -0,0 +1,60 @@
+module PhoneBook
+
+open System.IO
+
+type Name = string
+type PhoneNumber = string
+type FilePath = string
+
+type Record = Name * PhoneNumber
+type Records = Record list
+type PhoneBook = Records
+
+type Command =
+    | Exit
+    | AddRecord of Record
+    | FindPhoneNumber of Name
+    | FindName of PhoneNumber
+    | GetAll
+    | SaveToFile of FilePath
+    | ReadFromFile of FilePath
+
+type Output =
+    | Message of string
+    | FoundPhoneNumber of PhoneNumber option
+    | FoundName of Name option
+    | All of Records
+
+let unwrap nth option =
+    match option with
+    | None -> None
+    | Some tuple -> Some (nth tuple)
+
+let saveToFile (filePath: string) (phoneBook: PhoneBook) =
+    use writer = new StreamWriter (filePath)
+    for name, number in phoneBook do
+        writer.WriteLine (name + " " + number)
+
+let readFromFile (filePath: string) =
+    File.ReadAllLines filePath
+    |> Array.toList
+    |> List.map (fun line ->
+        let words = line.Split ' '
+        match words with
+        | [| name; number|] -> Record (name, number)
+        | _ -> raise (InvalidDataException "invalid record format")
+    )
+
+let execute (command: Command) (phoneBook: PhoneBook) =
+    match command with
+    | Exit -> exit 0
+    | AddRecord record -> Message "record was added", record :: phoneBook
+    | FindPhoneNumber name ->
+        FoundPhoneNumber (List.tryFind (fst >> ( = ) name) phoneBook |> unwrap snd), phoneBook
+    | FindName number ->
+        FoundName (List.tryFind (snd >> ( = ) number) phoneBook |> unwrap fst), phoneBook
+    | GetAll -> All phoneBook, phoneBook
+    | SaveToFile filePath ->
+        saveToFile filePath phoneBook
+        Message "phone book was written successfully", phoneBook
+    | ReadFromFile filePath -> Message "phone book was read successfully", readFromFile filePath
---
+++ Tasks/PhoneBook/PhoneBook/Program.fs
@@ -0,0 +1,53 @@
+﻿open System
+
+open PhoneBook
+
+printf "
+    Phone Book
+    ----------
+    Commands:
+        exit: exit the app
+        add {name} {number}: add a record to the book
+        find number {name}: find a phone number by the given name
+        find name {number}: find a name by the given phone number
+        get all: print all of the records in the book
+        save to {filePath}: save records to the given file
+        read from {filePath}: read records from the given file\n
+"
+
+let getCommand () =
+    let input = Console.ReadLine()
+    let words = input.Split ' '
+    match words with
+    | [| "exit" |] -> Some Exit
+    | [| "add"; name; number |] -> Some (AddRecord (name, number))
+    | [| "find"; "number"; name |] -> Some (FindPhoneNumber name)
+    | [| "find"; "name"; number |] -> Some (FindName number)
+    | [| "get"; "all" |] -> Some GetAll
+    | [| "save"; "to"; filePath |] -> Some (SaveToFile filePath)
+    | [| "read"; "from"; filePath |] -> Some (ReadFromFile filePath)
+    | _ -> None
+
+let handleOutput output =
+    match output with
+    | Message msg -> printfn "%s\n" msg
+    | FoundName (Some value) | FoundPhoneNumber (Some value) -> printfn "%s\n" value
+    | FoundName None -> printf "record with such number was not found\n"
+    | FoundPhoneNumber None -> printf "record with such name was not found\n"
+    | All records ->
+        for record in records do
+            printfn "%s %s\n" <|| record
+
+let rec run phoneBook =
+    printf "-> "
+    let command = getCommand()
+    match command with
+    | Some command ->
+        let output, phoneBook = execute command phoneBook
+        handleOutput output
+        run phoneBook
+    | None ->
+        printf "Unrecognized command\n"
+        run phoneBook
+
+run []
