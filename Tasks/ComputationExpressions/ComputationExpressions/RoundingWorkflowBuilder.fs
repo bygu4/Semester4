ae4ff65f7de49887f65fc52b1fe4b4eeabab2e35
bygu4 <sacha05bugaev@gmail.com>
2025-03-28 21:12:03 +0300

    add workflow builders

---
+++ Tasks/ComputationExpressions/ComputationExpressions/RoundingWorkflowBuilder.fs
@@ -0,0 +1,11 @@
+﻿namespace ComputationExpressions
+
+open System
+
+type RoundingWorkflowBuilder (precision: int) =
+    do ArgumentOutOfRangeException.ThrowIfNegative precision
+
+    member _.Bind (value: float, cont: float -> float) =
+        Math.Round (value, precision) |> cont
+
+    member _.Return value: float = value
