f68964b8517e03d8f1eafeec3ef41e60ca18596b
bygu4 <sacha05bugaev@gmail.com>
2025-04-13 21:20:55 +0300

    add tests

--- Tasks/MiniCrawler/MiniCrawler/MiniCrawler.fs
+++ Tasks/MiniCrawler/MiniCrawler/MiniCrawler.fs
@@ -1,11 +1,10 @@
-﻿module Crawler
+﻿module MiniCrawler
 
 open System.Net.Http
 open System.Text.RegularExpressions
 
-let referencePattern = @"http(s?)://(.*)"
-let quotedReferencePattern = "\"" + referencePattern + "\""
-let linkPattern = "<a href=" + quotedReferencePattern + ">"
+let referencePattern = @"(""http(s?)://[^""]*"")|('http(s?)://[^']*')"
+let linkPattern = "<a href=" + referencePattern + ">"
 
 /// Gets html content of the resource with the given `address` as a string.
 let getContent (address: string) =
@@ -15,7 +14,7 @@ let getContent (address: string) =
     } |> Async.AwaitTask
 
 /// Asynchronously gets html content for each of the pages defined by given `addresses`.
-let getContents (addresses: string seq) =
+let getAllContents (addresses: string seq) =
     addresses
     |> Seq.map getContent
     |> Async.Parallel
@@ -23,17 +22,16 @@ let getContents (addresses: string seq) =
 /// Gets all page references in the given html `content`.
 let getAllReferences (content: string) =
     Regex.Matches(content, linkPattern)
-    |> Seq.map _.ToString()
-    |> Seq.map (fun link -> Regex.Match(link, referencePattern))
-    |> Seq.map _.ToString()
+    |> Seq.map (fun link -> Regex.Match(link.Value, referencePattern))
+    |> Seq.map (fun href -> href.Value.Substring(1, href.Length - 2))
 
 /// Gets info about each of the resource referenced by the page with the given `address`.
 /// Info contains the reference address and the length of the referenced page.
-let getAllReferencedContents (address: string) =
+let getAllLinksInfo (address: string) =
     async {
         let! content = getContent address
         let references = getAllReferences content
-        let! referenceContents = getContents references
+        let! referenceContents = getAllContents references
         let lengths = referenceContents |> Seq.map _.Length
         return Seq.zip references lengths
     }
@@ -44,7 +42,7 @@ let printAllLinksInfo (address: string) =
     let printInfo (address: string, length: int) =
         printfn "%s --- %d" address length
 
-    getAllReferencedContents address
+    getAllLinksInfo address
     |> Async.RunSynchronously
     |> Seq.map printInfo
     |> ignore
