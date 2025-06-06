c10d02fb97a5d5e69b0782bce893914d67fb4625
bygu4 <sacha05bugaev@gmail.com>
2025-06-06 12:14:39 +0300

    add count property, add tests

--- Tasks/Test/Test/Queue.fs
+++ Tasks/Test/Test/Queue.fs
@@ -8,10 +8,10 @@ type Node<'a>(value: 'a, next: Node<'a> option) =
     let mutable next = next
 
     /// Get the value of the node.
-    member _.Value () = value
+    member _.Value: 'a = value
 
     /// Get the next node of the current one.
-    member _.Next () = next
+    member _.Next: Node<'a> option = next
 
     /// Set the next node of the current one.
     member _.SetNext (node: Node<'a> option) = next <- node
@@ -20,25 +20,31 @@ type Node<'a>(value: 'a, next: Node<'a> option) =
 type Queue<'a>() =
     let mutable head: Node<'a> option = None
     let mutable tail: Node<'a> option = None
+    let mutable count = 0
 
-    /// Add the given value to the queue.
+    /// Get the size of the queue.
+    member _.Count: int = count
+
+    /// Add the given `value` to the queue.
     member _.Enqueue (value: 'a): unit =
         let next = Node (value, None)
         match tail with
         | Some node ->
             node.SetNext <| Some next
-            tail <- Some node
+            tail <- Some next
         | None ->
             head <- Some next
             tail <- Some next
+        count <- count + 1
 
     /// Remove the first element from the queue and return its value.
     member _.Dequeue (): 'a =
         match head with
         | Some node ->
-            let res = node.Value ()
-            head <- node.Next ()
+            let res = node.Value
+            head <- node.Next
             if head.IsNone then tail <- None
+            count <- count - 1
             res
         | None ->
             raise (new InvalidOperationException "Error on dequeue: queue was empty")
